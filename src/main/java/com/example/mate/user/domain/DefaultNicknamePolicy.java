e6fa78562641a3274b20fb4caac6994cea9cd989
jjikky <kok5417@skuniv.ac.kr>
2024-12-12 17:38:57 +0900

    feat: 유저 도메인 및 상태 관리 로직 추가 #4
    - 유저 엔티티 및 상태 열거형 정의
    - 유저 닉네임 생성 정책 추가
    - 유저 상태 관리 메서드 추가

---
+++ src/main/java/com/example/mate/user/domain/DefaultNicknamePolicy.java
@@ -0,0 +1,20 @@
+package com.example.mate.user.domain;
+
+import java.util.UUID;
+
+public class DefaultNicknamePolicy {
+
+    private static final int RANDOM_LENGTH = 8;
+    private static final String PREFIX = "DAO-";
+
+    public static String generatedRandomString() {
+        String randomString = generatedSubStringUUID();
+        return PREFIX + randomString;
+    }
+
+    private static String generatedSubStringUUID() {
+        return UUID.randomUUID().toString()
+                .replaceAll("-", "")
+                .substring(0, DefaultNicknamePolicy.RANDOM_LENGTH);
+    }
+}
---
+++ src/main/java/com/example/mate/user/domain/User.java
@@ -0,0 +1,88 @@
+package com.example.mate.user.domain;
+
+import com.example.mate.common.domain.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+public class User extends BaseTimeEntity {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "kakao_id")
+    private String kakaoId;
+
+    private String nickname;
+
+    @Column(name = "profile_url")
+    private String profileUrl;
+
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+
+    private String reasons;
+
+    @Column(columnDefinition = "TEXT", name = "reason_detail")
+    private String reasonDetail;
+
+    @Builder
+    public User(String kakaoId) {
+        this.kakaoId = kakaoId;
+        this.nickname = DefaultNicknamePolicy.generatedRandomString();
+        this.status = UserStatus.ACTIVE_FIRST_LOGIN;
+    }
+
+    public User(Long id, String kakaoId, String nickname, String profileUrl, UserStatus status) {
+        this.id = id;
+        this.kakaoId = kakaoId;
+        this.nickname = nickname;
+        this.profileUrl = profileUrl;
+        this.status = status;
+    }
+
+    public void updateUserInfo(String nickname, String profileUrl) {
+        if (nickname != null) {
+            this.nickname = nickname;
+        }
+        if (profileUrl != null) {
+            this.profileUrl = profileUrl;
+        }
+        if (status.isFirstLogin()) {
+            status = UserStatus.ACTIVE;
+        }
+    }
+
+    public boolean isDeletedUser() {
+        return status == UserStatus.DELETED;
+    }
+
+    public void updateActiveStatus() {
+        this.status = UserStatus.ACTIVE;
+    }
+
+    public void withdrawWithAddReason(List<String> stringReasonTypeList, String detail) {
+        this.reasons = stringReasonTypeList.stream()
+                .map(ReasonType::getReasonTypeByString)
+                .toList()
+                .toString();
+        this.reasonDetail = detail;
+        this.status = UserStatus.DELETED;
+    }
+}
