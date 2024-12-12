86ec05029c07065e7f52d64ce46b9110c819059e
jjikky <kok5417@skuniv.ac.kr>
2024-12-12 17:42:19 +0900

    feat: 유저 정보 조회 API 컨트롤러 구현 #4
    - 단일 유저 정보를 조회하는 GET /api/v1/users/{userId} 엔드포인트 추가
    - API 응답 형식 통일을 위한 ApiResponse 래퍼 클래스 사용

--- src/main/java/com/example/mate/user/presentation/UserController.java
+++ src/main/java/com/example/mate/user/presentation/UserController.java
@@ -1,7 +1,12 @@
 package com.example.mate.user.presentation;
 
+import com.example.mate.common.response.ApiResponse;
 import com.example.mate.user.application.UserService;
+import com.example.mate.user.application.dto.UserInfoResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<ApiResponse<UserInfoResponseDto>> getUserInfo(
+            @PathVariable Long userId
+    ) {
+        return ResponseEntity.ok(new ApiResponse<>(
+                "LOAD_USER_INFO_SUCCESS",
+                userService.getUserInfoWithId(userId)
+        ));
+    }
 }
