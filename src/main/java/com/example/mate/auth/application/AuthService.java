5bb611b49b6eb75bae5011803847a351332fa40f
rudwns7723 <rudwns7723@naver.com>
2024-12-19 17:51:02 +0900

    feat: 카카오 로그인 db 저장 #10

--- src/main/java/com/example/mate/auth/application/AuthService.java
+++ src/main/java/com/example/mate/auth/application/AuthService.java
@@ -1,6 +1,10 @@
 package com.example.mate.auth.application;
 
 import com.example.mate.auth.application.dto.TokenResponseDto;
+import com.example.mate.auth.domain.Token;
+import com.example.mate.auth.domain.repository.TokenRepository;
+import com.example.mate.user.application.UserService;
+import com.example.mate.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,8 +12,23 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AuthService {
 
+    private final UserService userService;
+    private final TokenProvider tokenProvider;
+    private final TokenExtractor tokenExtractor;
+    private final TokenRepository tokenRepository;
+
+
     public TokenResponseDto loginOrRegister(String oAuthId) {
-        TokenResponseDto dto = new TokenResponseDto("aa", "bb");
-        return dto;
+        User findUser = userService.getOrRegisterByOAuthId(oAuthId);
+
+        Token newToken = Token.builder()
+                .userId(findUser.getId())
+                .build();
+
+        tokenRepository.save(newToken);
+
+        String accessToken = tokenProvider.generatedAccessToken(newToken.getUserId());
+        String refreshToken = tokenProvider.generatedRefreshToken(newToken.getTokenId());
+        return TokenResponseDto.of(accessToken, refreshToken);
     }
 }
