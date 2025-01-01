c7c8ad548b3669331495be90add2ade8dd401af9
jjikky <kok5417@skuniv.ac.kr>
2025-01-01 15:33:46 +0900

    feat: 상품 상세 조회 API 구현 #23
    - 상품 상세 정보를 조회할 수 있는 API를 구현
    - 상품 조회 시 연관된 태그와 작성자 정보를 함께 조회하도록 구현
    - 좋아요 수와 리뷰 수는 추후 구현 예정

--- src/main/java/com/example/mate/product/application/ProductService.java
+++ src/main/java/com/example/mate/product/application/ProductService.java
@@ -1,20 +1,28 @@
 package com.example.mate.product.application;
 
+import static com.example.mate.product.exception.ProductExceptionType.PRODUCT_NOT_FOUND_EXCEPTION;
+
 import com.example.mate.product.application.dto.ProductCreateRequestDto;
+import com.example.mate.product.application.dto.ProductDetailResponseDto;
 import com.example.mate.product.application.dto.ProductIdResponseDto;
 import com.example.mate.product.domain.Product;
 import com.example.mate.product.domain.Tag;
 import com.example.mate.product.domain.repository.ProductRepository;
+import com.example.mate.product.exception.ProductException;
 import com.example.mate.user.application.UserService;
 import com.example.mate.user.domain.User;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
+
+    private static final Logger log = LoggerFactory.getLogger(ProductService.class);
 
     private final UserService userService;
     private final TagService tagService;
@@ -38,5 +46,19 @@ public class ProductService {
         Product savedProduct = productRepository.save(newProduct);
 
         return new ProductIdResponseDto(savedProduct.getId());
+    }
+
+    @Transactional
+    public ProductDetailResponseDto getProductById(Long productId) {
+        Product findProduct = findProductById(productId);
+        // TODO : count 변경
+        // TODO : 좋아요 상태 적용
+        return ProductDetailResponseDto.of(findProduct, 1L, 1L, false);
+    }
+
+
+    private Product findProductById(Long productId) {
+        return productRepository.findByIdWithUserAndTags(productId)
+                .orElseThrow(() -> new ProductException(PRODUCT_NOT_FOUND_EXCEPTION));
     }
 }
