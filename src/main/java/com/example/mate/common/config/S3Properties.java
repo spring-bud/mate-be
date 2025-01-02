7c513495c254b7a69bb9b16d2af4ff60de2e8e79
jjikky <kok5417@skuniv.ac.kr>
2025-01-02 17:10:14 +0900

    chore: S3 설정 추가 #26
    - S3 관련 설정 프로퍼티 클래스 구현
    - S3 클라이언트 설정 추가
    - application.yml에 S3 관련 설정 추가

---
+++ src/main/java/com/example/mate/common/config/S3Properties.java
@@ -0,0 +1,14 @@
+package com.example.mate.common.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "cloud.aws.s3")
+public record S3Properties(
+        int cacheTimeSeconds,
+        String bucketName,
+        String accessKey,
+        String secretKey,
+        String imageUrl,
+        String region
+) {
+}
