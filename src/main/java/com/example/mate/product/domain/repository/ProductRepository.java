871f0e6a6b1104b2664213d79f4e3f3a948f6876
rudwns7723 <rudwns7723@naver.com>
2025-01-12 15:04:54 +0900

    feat: ProductRepository 프로덕트 리스트 쿼리 추가

--- src/main/java/com/example/mate/product/domain/repository/ProductRepository.java
+++ src/main/java/com/example/mate/product/domain/repository/ProductRepository.java
@@ -1,13 +1,12 @@
 package com.example.mate.product.domain.repository;
 
 import com.example.mate.product.domain.Product;
-
 import io.lettuce.core.dynamic.annotation.Param;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -18,4 +17,10 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             "LEFT JOIN FETCH pt.tag " +
             "WHERE p.id = :productId")
     Optional<Product> findByIdWithUserAndTags(@Param("productId") Long productId);
+
+    @Query("SELECT p FROM Product p " +
+            "LEFT JOIN FETCH p.user " +
+            "LEFT JOIN FETCH p.productTags pt " +
+            "LEFT JOIN FETCH pt.tag ")
+    List<Product> findWithUserAndTags();
 }
