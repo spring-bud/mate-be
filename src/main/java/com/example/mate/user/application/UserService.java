92292f0a9b78d848bc9973e33bad37a7fd5bacc3
rudwns7723 <rudwns7723@naver.com>
2025-01-15 13:57:54 +0900

    feat: UserService 인기 유저 서비스 추가

--- src/main/java/com/example/mate/user/application/UserService.java
+++ src/main/java/com/example/mate/user/application/UserService.java
@@ -1,16 +1,21 @@
 package com.example.mate.user.application;
 
-import com.example.mate.user.application.dto.UserInfoRequestDto;
-import com.example.mate.user.application.dto.UserInfoResponseDto;
+import com.example.mate.product.domain.repository.LikeRepository;
+import com.example.mate.review.domain.repository.ReviewRepository;
+import com.example.mate.user.application.dto.*;
 import com.example.mate.user.domain.Stack;
 import com.example.mate.user.domain.User;
 import com.example.mate.user.domain.repository.UserRepository;
 import com.example.mate.user.exception.UserException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.example.mate.user.exception.UserExceptionType.NOT_EXIST_USER;
 
@@ -19,6 +24,8 @@ import static com.example.mate.user.exception.UserExceptionType.NOT_EXIST_USER;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final LikeRepository likeRepository;
+    private final ReviewRepository reviewRepository;
     private final StackService stackService;
 
     public UserInfoResponseDto getUserInfoWithId(Long userId) {
@@ -78,5 +85,28 @@ public class UserService {
 
         User updateUser = userRepository.save(findUser);
         return UserInfoResponseDto.of(updateUser);
+    }
+
+    private List<PopularityUserResponseDto> getPopularityUsers() {
+        Pageable pageRequest = PageRequest.of(0, 5, Sort.by(Sort.Order.desc("count")));
+        return likeRepository.countPopularityUser(pageRequest);
+    }
+
+    public List<PopularityUserInfoResponseDto> getPopularityUsersInfo() {
+
+        List<PopularityUserInfoResponseDto> popularityUsersInfo = getPopularityUsers().stream()
+                .map(popularityUser -> {
+                    User findUser = userRepository.findByIdAndStatusIsNotDeleted(popularityUser.userId())
+                            .orElseThrow(() -> new UserException(NOT_EXIST_USER));
+
+                    PopularityUserReviewResponseDto userReviewInfo = reviewRepository.findByuserId(popularityUser.userId());
+
+                    return PopularityUserInfoResponseDto.of(
+                            findUser,
+                            userReviewInfo
+                    );
+                })
+                .collect(Collectors.toList());
+        return popularityUsersInfo;
     }
 }
