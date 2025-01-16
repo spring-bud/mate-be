8484ef48503dca9e9389d48453914b13fb9f95dd
rudwns7723 <rudwns7723@naver.com>
2025-01-16 10:45:28 +0900

    feat: Review 저장소 쿼리 추가

--- src/main/java/com/example/mate/review/domain/repository/ReviewRepository.java
+++ src/main/java/com/example/mate/review/domain/repository/ReviewRepository.java
@@ -1,6 +1,7 @@
 package com.example.mate.review.domain.repository;
 
 import com.example.mate.review.domain.Review;
+import com.example.mate.user.application.dto.PopularityUserReviewResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,24 +12,44 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     @Query("""
             SELECT r FROM Review r
              LEFT JOIN FETCH r.product p
+             LEFT JOIN FETCH r.user u
              WHERE p.user.id = :userId
-             ORDER BY r.createdAt DESC
+             AND r.status != 'DELETED'
+             ORDER BY r.createdAt DESC  
             """)
     List<Review> findByUserId(Long userId);
 
     @Query("""
             SELECT r FROM Review r
              LEFT JOIN FETCH r.product p
+             LEFT JOIN FETCH r.user u
              WHERE p.id = :productId
+             AND r.status != 'DELETED'
              ORDER BY r.createdAt DESC
             """)
     List<Review> findByProductId(Long productId);
 
-    Optional<Review> findByIdAndUserId(Long reviewId, Long userId);
+    Optional<Review> findById(Long reviewId);
 
     @Query("""
             SELECT COUNT(r) FROM Review r
              WHERE r.product.id = :productId
+               AND r.status != 'DELETED'
             """)
     Long countReviewByProductId(Long productId);
+
+    @Query("""
+            SELECT new com.example.mate.user.application.dto.PopularityUserReviewResponseDto(COUNT(r), AVG(r.star)) FROM Review r
+             LEFT JOIN r.product p
+             WHERE p.user.id = :userId
+             AND r.status != 'DELETED'
+            """)
+    PopularityUserReviewResponseDto findByUserIdReviewStats(Long userId);
+
+    @Query("""
+            UPDATE Review r
+             SET r.status = 'DELETED'
+             WHERE r.product.id = :productId
+            """)
+    void updateStatusdByProductId(Long productId);
 }
