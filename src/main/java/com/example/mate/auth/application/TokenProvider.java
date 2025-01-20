81151d35951e3ed92e6d1ac748445b2a4cbd82ae
rudwns7723 <rudwns7723@naver.com>
2025-01-20 11:07:56 +0900

    feat: TokenProvider 프로필 url, 닉네임 값 추가

--- src/main/java/com/example/mate/auth/application/TokenProvider.java
+++ src/main/java/com/example/mate/auth/application/TokenProvider.java
@@ -2,7 +2,7 @@ package com.example.mate.auth.application;
 
 public interface TokenProvider {
 
-    String generatedAccessToken(Long userId);
+    String generatedAccessToken(Long userId, String userProfileUrl, String userNickname);
 
     String generatedRefreshToken(String tokenId);
 }
