fb34026e039e86ca58b52c562b2c358f89f6ef94
rudwns7723 <rudwns7723@naver.com>
2025-01-20 14:58:02 +0900

    feat: ProductRepository 유저 삭제시 유저가 생성한 프로덕트 삭제 쿼리 추가

--- src/main/java/com/example/mate/product/domain/repository/ProductRepository.java
+++ src/main/java/com/example/mate/product/domain/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package com.example.mate.product.domain.repository;
 import com.example.mate.product.domain.Product;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -16,14 +17,16 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             "LEFT JOIN FETCH p.productTags pt " +
             "LEFT JOIN FETCH pt.tag " +
             "WHERE p.id = :productId " +
-            "AND u.status != 'DELETED'")
+            "AND u.status != 'DELETED'" +
+            "AND p.status != 'DELETED'")
     Optional<Product> findByIdWithUserAndTags(@Param("productId") Long productId);
 
     @Query("SELECT p FROM Product p " +
             "LEFT JOIN FETCH p.user u " +
             "LEFT JOIN FETCH p.productTags pt " +
             "LEFT JOIN FETCH pt.tag " +
-            "WHERE u.status != 'DELETED'")
+            "WHERE u.status != 'DELETED'" +
+            "AND p.status != 'DELETED'")
     List<Product> findWithUserAndTags();
 
     @Query("SELECT p FROM Product p " +
@@ -31,6 +34,15 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             "LEFT JOIN FETCH p.productTags pt " +
             "LEFT JOIN FETCH pt.tag " +
             "WHERE p.user.id = :userId " +
-            "AND u.status != 'DELETED'")
+            "AND u.status != 'DELETED'" +
+            "AND p.status != 'DELETED'")
     List<Product> findByUserId(@Param("userId") Long userId);
+
+    @Modifying
+    @Query("""
+            UPDATE Product p
+             SET p.status = 'DELETED'
+             WHERE p.user.id = :userId
+            """)
+    void updateStatusdByUsertId(Long userId);
 }
