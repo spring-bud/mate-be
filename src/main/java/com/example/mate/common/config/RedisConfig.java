3d278a7ade22b893ed6f89075b8f5e778f654289
rudwns7723 <rudwns7723@naver.com>
2025-01-30 17:27:38 +0900

    feat: RedisConfig 설정

--- src/main/java/com/example/mate/common/config/RedisConfig.java
+++ src/main/java/com/example/mate/common/config/RedisConfig.java
@@ -1,19 +1,41 @@
 package com.example.mate.common.config;
 
+import com.example.mate.chat.infrastructure.redis.RedisChatMessageListener;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @RequiredArgsConstructor
 public class RedisConfig {
 
+    private static final String CHATTING_CHANNEL_TOPIC = "chatting:channel";
+
+    private final RedisChatMessageListener chatMessageListener;
+
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+    public RedisMessageListenerContainer redisMessageListenerContainer(
+            RedisConnectionFactory connectionFactory
+    ) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        chatSubscribe(container);
+        return container;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(
+            ObjectMapper objectMapper,
+            RedisConnectionFactory redisConnectionFactory
+    ) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
 
         template.setConnectionFactory(redisConnectionFactory);
@@ -24,8 +46,15 @@ public class RedisConfig {
 
         template.setHashKeySerializer(new StringRedisSerializer());
 
-        template.setHashValueSerializer(new GenericToStringSerializer<>(Object.class));
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(objectMapper, String.class));
 
         return template;
+    }
+
+    private void chatSubscribe(RedisMessageListenerContainer container) {
+        container.addMessageListener(
+                chatMessageListener,
+                new ChannelTopic(CHATTING_CHANNEL_TOPIC)
+        );
     }
 }
