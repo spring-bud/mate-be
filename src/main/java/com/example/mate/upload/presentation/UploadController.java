8fe89fe606eb742eec2e7db87763e417070e2997
rudwns7723 <rudwns7723@naver.com>
2025-04-19 22:55:13 +0900

    feat : S3ImageClient uploadFile 추가

--- src/main/java/com/example/mate/upload/presentation/UploadController.java
+++ src/main/java/com/example/mate/upload/presentation/UploadController.java
@@ -1,14 +1,13 @@
 package com.example.mate.upload.presentation;
 
+import com.example.mate.chat.application.dto.ChatMessageDto;
 import com.example.mate.common.response.ApiResponse;
 import com.example.mate.upload.application.UploadService;
 import com.example.mate.upload.application.dto.UploadImageResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -25,5 +24,15 @@ public class UploadController {
         return ResponseEntity.ok(new ApiResponse<>(
                 uploadService.upload(file)
         ));
+    }
+
+    @PostMapping
+    public void uploadFile(
+            @RequestPart("file") MultipartFile file,
+            @RequestPart("messageDto") ChatMessageDto messageDto,
+            @AuthenticationPrincipal Long userId
+    ) {
+        uploadService.uploadFile(file, userId, messageDto);
+
     }
 }
