8f4154e025c0ebe7a942093123937dc6699def95
rudwns7723 <rudwns7723@naver.com>
2025-05-31 21:42:56 +0900

    refactor : ReviewUserResponseDto 리뷰 작성자 아이디 추가

--- src/main/java/com/example/mate/review/application/dto/ReviewUserResponseDto.java
+++ src/main/java/com/example/mate/review/application/dto/ReviewUserResponseDto.java
@@ -6,6 +6,7 @@ import com.example.mate.user.domain.User;
 public record ReviewUserResponseDto(
         Long id,
         Double star,
+        Long review_user_id,
         String content,
         String nickname,
         String profileUrl
@@ -14,6 +15,7 @@ public record ReviewUserResponseDto(
         return new ReviewUserResponseDto(
                 review.getId(),
                 review.getStar(),
+                review.getUser().getId(),
                 review.getContent(),
                 user.getNickname(),
                 user.getProfileUrl()
